e28db7bd9782f5ac61ce73a777113562a98f5d7f
Nicodeveloper97 <nicoiglesiasdeveloper@gmail.com>
2024-09-15 15:04:07 -0300

    mas cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Technologies from "./components/Technologies"
-import AboutMe from "./components/AboutMe"
-import Experiencia from "./components/Experiencie"
-import Proyecto from "./components/Proyectos"
-import Contacto from "./components/Contact"
-import Footer from "./components/Footer"
-
-
+import Technologies from "./components/Technologies";
+import AboutMe from "./components/AboutMe";
+import Experiencia from "./components/Experiencie";
+import Proyecto from "./components/Proyectos";
+import Contacto from "./components/Contact";
+import Footer from "./components/Footer";
+import ThankYou from './components/ThankYou'; 
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Experiencia />
-      <Proyecto />
-      <AboutMe/>
-      <Technologies />
-      <Contacto />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Experiencia />
+              <Proyecto />
+              <AboutMe />
+              <Technologies />
+              <Contacto />
+              <Footer />
+            </>
+          } />
+
+          <Route path="/thank-you" element={<ThankYou />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/ThankYou.tsx
@@ -0,0 +1,14 @@
+
+
+const ThankYou = () => {
+  return (
+    <section className="w-full h-screen flex items-center justify-center bg-gray-900 text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">¡Gracias por tu mensaje!</h1>
+        <p className="text-lg">Nos pondremos en contacto contigo lo antes posible.</p>
+      </div>
+    </section>
+  );
+};
+
+export default ThankYou;
